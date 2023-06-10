6efb05d2a49a212b58d8c37ec5296b3c1afa97f5
iamcheeseman <xanderswan32@gmail.com>
2023-06-09 21:21:38 -0400

    Portals are now the same theme as the substance giver

--- world/props/substancegiver/substancegiver.lua
+++ world/props/substancegiver/substancegiver.lua
@@ -2,6 +2,6 @@ Objects.create_type("SubstanceGiver", {
     sprite = Sprite.new("world/props/substancegiver/substancegiver.png", 1, 0),
 
     on_create = function(self)
-        Objects.instance_at("OriginalSubstance", self.x, self.y - 12)
+        Objects.instance_at("OriginalSubstance", self.x + 2.5, self.y - 12)
     end
 })
