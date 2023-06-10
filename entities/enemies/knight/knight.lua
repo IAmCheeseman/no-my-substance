232bf53c88b6fa614da262d3a3cb26af931fbd19
iamcheeseman <xanderswan32@gmail.com>
2023-06-10 11:15:35 -0400

    Knight works properly

--- entities/enemies/knight/knight.lua
+++ entities/enemies/knight/knight.lua
@@ -1,5 +1,27 @@
 local collision = require "entities.collide"
 
+local function flee(self, dt)
+    self.sprite:apply_animation(self.walk_animation)
+
+    local dir_x, dir_y = Vector.direction_between(self.x, self.y, self.target.x, self.target.y)
+
+    self.vel_x = math.lerp(self.vel_x, -dir_x * self.speed, self.accel * dt)
+    self.vel_y = math.lerp(self.vel_y, -dir_y * self.speed, self.accel * dt)
+
+    self.sprite.scale_x = self.target.x < self.x and -1 or 1
+
+    collision.move(self, "Solids", self.vel_x * dt, self.vel_y * dt)
+end
+
+local function attack(self, dt)
+    self.sprite:apply_animation(self.attack_animation)
+
+    self.vel_x = math.lerp(self.vel_x, 0, self.accel * dt)
+    self.vel_y = math.lerp(self.vel_y, 0, self.accel * dt)
+
+    collision.move(self, "Solids", self.vel_x * dt, self.vel_y * dt)
+end
+
 local function charge(self, dt)
     self.sprite:apply_animation(self.walk_animation)
 
@@ -11,6 +33,12 @@ local function charge(self, dt)
     self.sprite.scale_x = self.target.x < self.x and -1 or 1
 
     collision.move(self, "Solids", self.vel_x * dt, self.vel_y * dt)
+
+    local dist_to_player = Vector.distance_between(self.x, self.y, self.player.x, self.player.y)
+    if dist_to_player < 32 then
+        self.state = attack
+        self.timers.attack:start()
+    end
 end
 
 local function default(self, dt)
@@ -26,20 +54,46 @@ Objects.create_type_from("Knight", "Enemy", {
     corpse_sprite = Sprite.new("entities/enemies/knight/knightcorpse.png", 1, 0),
     idle_animation = Sprite.new_animation(1, 3, 10),
     walk_animation = Sprite.new_animation(4, 6, 10),
+    attack_animation = Sprite.new_animation(7, 7, 0),
 
     speed = 90,
     accel = 3,
 
     state = default,
 
+    on_attack_over = function(self)
+        if self.state == default then
+            return
+        end
+        self.sword:swing()
+        self.state = flee
+        self.timers.flee:start()
+    end,
+    on_flee_over = function(self)
+        if self.state == default then
+            return
+        end
+        self.state = charge
+    end,
+
     on_create = function(self)
         self.sprite:apply_animation(self.idle_animation)
         self.sprite.offset_y = math.floor(self.sprite.texture:getHeight() / 2)
 
+        self:create_timer("attack", self.on_attack_over, 0.5)
+        self:create_timer("flee", self.on_flee_over, 1)
+
         self:call_from_base("on_create")
+
+        self.sword = Objects.instance_at("KnightSword", self.x, self.y)
+        self.sword.target = self
     end,
     on_update = function(self, dt)
         self:call_from_base("on_update", dt)
         self:state(dt)
     end,
+
+    on_death = function(self)
+        Objects.destroy(self.sword)
+    end
 })
---
+++ entities/enemies/knight/sword.lua
@@ -0,0 +1,45 @@
+Objects.create_type("KnightSword", {
+    sprite = Sprite.new("entities/enemies/knight/sword.png", 6, 10),
+    idle_animation = Sprite.new_animation(1, 1, 0),
+    swing_animation = Sprite.new_animation(2, 6, 20),
+    target = nil,
+
+    swing = function(self)
+        self.sprite:apply_animation(self.swing_animation)
+
+        local de_x, de_y = Vector.direction_between(self.x, self.y, self.player.x, self.player.y)
+        local dir_x, dir_y = Vector.rotated(1, 0, self.sprite.rotation)
+        local dist = Vector.distance_between(self.x, self.y, self.player.x, self.player.y)
+        local dot = Vector.dot(dir_x, dir_y, de_x, de_y)
+        if dot > 0 and dist < 32 then
+            self.player:take_damage(2.5, dir_x, dir_y)
+        end
+    end,
+
+    on_create = function(self)        
+        self.sprite:apply_animation(self.idle_animation)
+        self.sprite.offset_x = -5
+        self.sprite.offset_y = math.floor(self.sprite.texture:getHeight() / 2)
+        self.sprite.center = false
+
+        self:create_timer("cooldown", nil, 0.7)
+
+        self.player = Objects.grab("Player")
+    end,
+    on_update = function(self, dt)
+        self.sprite.rotation = Vector.angle_between(self.x, self.y, self.player.x, self.player.y)
+        self.sprite.scale_y = self.x > self.player.x and -1 or 1
+
+        self.x = self.target.x
+        self.y = self.target.y - self.target.sprite.texture:getHeight() / 2
+
+        self.depth = self.target.depth + 1
+
+        if self.sprite.frame == self.swing_animation.anim_end then -- Swing once
+            self.sprite:apply_animation(self.idle_animation)
+        end
+    end,
+    on_draw = function(self)
+        self.sprite:draw(self.x, self.y)
+    end,
+})
