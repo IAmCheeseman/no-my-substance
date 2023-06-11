91a4dee78da4507cc7b35a758b05f9f7426c4ee7
iamcheeseman <xanderswan32@gmail.com>
2023-06-11 11:23:39 -0400

    Better tile collision

--- entities/collide.lua
+++ entities/collide.lua
@@ -10,8 +10,9 @@ local function can_walk_on_cell(cell, valid_cells)
     return false
 end
 
+--TODO: MAKE `valid_cells` NOT SUCK
 function module.move(object, with, mx, my, valid_cells)
-    valid_cells = valid_cells or { 0, 3 }
+    valid_cells = valid_cells or Room.valid_cells
     local check_x = object.x + mx
     local check_y = object.y + my
     if can_walk_on_cell(Room.get_cell(with, check_x, object.y), valid_cells) then
