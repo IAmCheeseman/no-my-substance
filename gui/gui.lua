001f412c9933cf65c7acc0365d4caf33f56ce01e
iamcheeseman <xanderswan32@gmail.com>
2023-06-11 20:21:08 -0400

    Pause menu

--- gui/gui.lua
+++ gui/gui.lua
@@ -36,4 +36,83 @@ function module.outlined_text(what, x, y, limit, align, outlinec, textc, size)
     love.graphics.printf(what, x, y, limit, align)
 end
 
+if not Objects.does_type_exist("Label") then
+    Objects.create_type("Label", {
+        text = "Label",
+        font = module.font,
+        foreground_color = { 1, 1, 1 },
+
+        on_gui = function(self)
+            love.graphics.setFont(self.font)
+
+            local tw, th = self.font:getWidth(self.text), self.font:getHeight(self.text)
+
+            love.graphics.setColor(self.foreground_color[1], self.foreground_color[2], self.foreground_color[3])
+            love.graphics.print(self.text, self.x - tw / 2, self.y - th / 2)
+        end
+    })
+end
+
+if not Objects.does_type_exist("Button") then
+    Objects.create_type("Button", {
+        text = "Button",
+        font = module.font,
+        text_padding = 3,
+        hover_scale = 1.25,
+        scale = 1,
+        background_color = { 0, 0, 0 },
+        foreground_color = { 1, 1, 1 },
+
+        get_size = function(self)
+            local tw, th = self.font:getWidth(self.text), self.font:getHeight(self.text)
+            return tw + self.text_padding, th + self.text_padding
+        end,
+
+        is_hovered = function(self)
+            local mx, my = love.mouse.getWindowPosition()
+            local w, h = self:get_size()
+            local x, y = self.x - w / 2, self.y - h / 2
+
+            return mx > x and mx < x + w and
+                    my > y and my < y + h
+        end,
+
+        on_create = function(self)
+            self.text = string.upper(self.text)
+        end,
+
+        on_update = function(self, dt)
+            local hovered = self:is_hovered()
+
+            if hovered then
+                self.target_scale = self.hover_scale
+            else
+                self.target_scale = 1
+            end
+
+            self.scale = math.lerp(self.scale, self.target_scale, 20 * dt)
+        end,
+
+        on_mouse_press = function(self, _, _, button, _, _)
+            if button == 1 and self:is_hovered() and self.on_click then
+                self.on_click()
+            end
+        end,
+
+        on_gui = function(self)
+            love.graphics.setFont(self.font)
+            
+            local tw, th = self:get_size()
+
+            local w, h = tw * self.scale, th * self.scale
+            local x, y = self.x - w / 2, self.y - h / 2
+
+            love.graphics.setColor(self.background_color[1], self.background_color[2], self.background_color[3])
+            love.graphics.rectangle("fill", x, y, w, h)
+            love.graphics.setColor(self.foreground_color[1], self.foreground_color[2], self.foreground_color[3])
+            love.graphics.print(self.text, x + self.text_padding / 2, y + self.text_padding / 2, 0, self.scale, self.scale)
+        end
+    })
+end
+
 return module
--- gui/pauser.lua
+++ gui/pauser.lua
@@ -1,17 +1,66 @@
 Objects.create_type("Pauser", {
     persistent = true,
-    depth = 10000,
+    depth = 50,
 
     pause_mode = "never",
     
-    on_draw = function(self)
-        if Objects.are_paused then
-            love.graphics.printf("PAUSED", Game.camera_x - 32, Game.camera_y, 64, "center")
-        end
+    ui_elements = {},
+
+    new_ui_element = function(self, type, x, y)
+        local element = Objects.instance_at(type, x, y)
+        element.pause_mode = "never"
+        element.depth = 51
+        table.insert(self.ui_elements, element)
+        return element
     end,
+
+    update_pause_menu = function(self)
+        if Objects.are_paused then
+            local title = self:new_ui_element("Label", 50, 60)
+            title.text = "PAUSED"
+
+            local continue = self:new_ui_element("Button", 50, 80)
+            continue.text = "Continue"
+            continue.on_click = function()
+                Objects.are_paused = false
+                self:update_pause_menu()
+            end    
+
+            local volume = self:new_ui_element("Button", 50, 100)
+            volume.text = love.audio.getVolume() == 0 and "Volume: Off" or "Volume: On"
+            volume.on_click = function()
+                if love.audio.getVolume() == 0 then
+                    love.audio.setVolume(1)
+                    volume.text = "VOLUME: ON"
+                else
+                    love.audio.setVolume(0)
+                    volume.text = "VOLUME: OFF"
+                end
+            end
+
+            local exit = self:new_ui_element("Button", 50, 120)
+            exit.text = "Exit"
+            exit.on_click = love.window.close
+        else
+            for _, v in ipairs(self.ui_elements) do
+                Objects.destroy(v)
+            end
+            self.ui_elements = {}
+        end
+    end,    
+
+    on_gui = function(self)
+        if not Objects.are_paused then
+            return
+        end
+        love.graphics.setColor(0, 0, 0, 0.5)
+        love.graphics.rectangle("fill", 25, 70, 50, 60)
+    end,
+
     on_key_press = function(self, key, _, is_repeat)
         if key == "escape" and not is_repeat then
             Objects.are_paused = not Objects.are_paused
+            self:update_pause_menu()
         end
     end
 })
