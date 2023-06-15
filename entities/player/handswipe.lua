e620af00ce58340590a86a7dde8eb77542221d24
iamcheeseman <xanderswan32@gmail.com>
2023-06-15 16:45:02 -0400

    Swing hits enemies once

--- entities/player/handswipe.lua
+++ entities/player/handswipe.lua
@@ -23,9 +23,7 @@ function hand_swipe:on_create()
     self.sprite.rotation = Vector.angle_between(self.x, self.y, mx, my)
 
     swing:play()
-end
 
-function hand_swipe:on_update(dt)
     local dir_x, dir_y = Vector.direction_between(self.x, self.y, love.mouse.getPosition())
 
     Objects.with("Enemy", function(other)
@@ -49,7 +47,9 @@ function hand_swipe:on_update(dt)
             hit:play()
         end
     end)
+end
 
+function hand_swipe:on_update(dt)
     if self.sprite.frame == 1 then
         Objects.destroy(self)
     end
